88b66d9d1e03077423620155548a48f9017adcc4
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-02-13 17:44:59 +0300

    feat:(authRouter)

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,11 +1,11 @@
 package routes
 
 import (
-	controller "github.com/Lubwama-Emmanuel/go_jwt_mongodb/controllers"
+	"github.com/Lubwama-Emmanuel/go_jwt_mongodb/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func AuthRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.POST("users/signUp", controller.SignUp)
-	incomingRoutes.POST("users/logIn", controller.LogIn)
+	incomingRoutes.POST("users/signUp", controllers.SignUp())
+	incomingRoutes.POST("users/logIn", controllers.LogIn())
 }
