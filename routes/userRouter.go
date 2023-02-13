9dd3657395010f2775aefdb71c55a44196115881
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-02-13 17:45:47 +0300

    feat(More about the user router)

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -4,11 +4,11 @@ import (
 	"github.com/Lubwama-Emmanuel/go_jwt_mongodb/middleware"
 	"github.com/gin-gonic/gin"
 
-	controller "github.com/Lubwama-Emmanuel/go_jwt_mongodb/controllers"
+	"github.com/Lubwama-Emmanuel/go_jwt_mongodb/controllers"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.Use(middleware.Authenticate())
-	incomingRoutes.GET("/users", controller.GetUsers())
-	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+	incomingRoutes.GET("/users", controllers.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
 }
